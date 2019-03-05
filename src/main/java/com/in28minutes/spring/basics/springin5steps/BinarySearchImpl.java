b48d7c65615711697658faaccb400563a9b5d7d5
Vicente Noriega <vicente.noriega@pouncecorp.com>
2019-03-05 17:09:41 -0600

    11. Step 6 - Dynamic auto wiring and Troubleshooting - @Primary.

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,29 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+
+	@Autowired
+	private SortAlgorithm sortAlgorithm;
+
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+
+	}
+
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		
+		int[] sortedNumbers = this.sortAlgorithm.sort(numbers);
+		
+		System.out.println(sortAlgorithm);
+		
+		return 3;
+
+	}
+
+}
