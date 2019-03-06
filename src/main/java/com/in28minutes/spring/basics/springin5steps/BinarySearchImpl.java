4c723a12fcdbadd401df938d3394a841d0fe220d
Vicente Noriega <vicente.noriega@pouncecorp.com>
2019-03-06 16:20:23 -0600

    Removing unnecesary code.

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -8,22 +8,11 @@ public class BinarySearchImpl {
 
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
-
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-
-	}
-
+	
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
-		
 		int[] sortedNumbers = this.sortAlgorithm.sort(numbers);
-		
 		System.out.println(sortAlgorithm);
-		
 		return 3;
-
 	}
 
 }
